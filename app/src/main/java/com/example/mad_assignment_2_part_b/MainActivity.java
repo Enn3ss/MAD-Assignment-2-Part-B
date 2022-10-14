89b8640b2f9d2b416aff46fe37ce0fb731e54b7a
Enn3ss <110088031+Enn3ss@users.noreply.github.com>
2022-10-14 12:45:24 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/mad_assignment_2_part_b/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mad_assignment_2_part_b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
